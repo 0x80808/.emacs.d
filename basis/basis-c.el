54c229c863f385e072b8955cb03a7623bb5a3cf6
吕程 <lvc@2345.com>
2017-04-22 23:01:45 +0800

    asd

---
+++ basis/basis-c.el
@@ -0,0 +1,42 @@
+
+;;c/c++ 配置
+;; 当 'c-mode 'c++-mode 'java-mode 'asm-mode 加载时，加载 ggtags mode
+;; (add-hook 'c-mode-common-hook
+;;          (lambda ()
+;;            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+;;              (require 'company-dabbrev))))
+
+;; company-dabbrev
+
+
+;; (require 'ggtags)
+
+
+;; (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+;; (define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+;; (define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+;; (define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+;; (define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+;; (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+;; (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+;; 加载 xcscope
+;; (add-hook 'c-mode-common-hook
+;;           (lambda ()
+;;             (when (derived-mode-p 'c-mode 'c++-mode)
+;;               (require 'xcscope))))
+;; (setq cscope-do-not-update-database t)
+
+;; (define-key global-map [(control f3)]  'cscope-set-initial-directory)
+;; (define-key global-map [(control f4)]  'cscope-unset-initial-directory)
+;; (define-key global-map [(control f5)]  'cscope-find-this-symbol)
+;; (define-key global-map [(control f6)]  'cscope-find-global-definition)
+;; (define-key global-map [(control f7)]  'cscope-find-global-definition-no-prompting)
+;; (define-key global-map [(control f8)]  'cscope-pop-mark)
+;; (define-key global-map [(control f9)]  'cscope-next-symbol)
+;; (define-key global-map [(control f10)] 'cscope-next-file)
+;; (define-key global-map [(control f11)] 'cscope-prev-symbol)
+;; (define-key global-map [(control f12)] 'cscope-prev-file)
+;; (define-key global-map [(meta f9)]     'cscope-display-buffer)
+;; (define-key global-map [(meta f10)]    'cscope-display-buffer-toggle)
+
+(provide 'basis-c)
